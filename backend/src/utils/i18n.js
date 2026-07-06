b98b3d8d889e3f96d9d86c55cc889e990f60aa19
waseemnoorisys-ship-it <waseem.noorisys@gmail.com>
2026-07-06 13:51:08 +0530

    successfully add the i18n and response in two lanuages french and english

---
+++ backend/src/utils/i18n.js
@@ -0,0 +1,40 @@
+const en = require("../locales/en.json");
+const fr = require("../locales/fr.json");
+
+const locales = { en, fr };
+
+function t(keyPath, lang, data) {
+  const locale = locales[lang] || locales["en"];
+  const keys = keyPath.split(".");
+  let value = locale;
+
+  for (const key of keys) {
+    if (value && typeof value === "object" && key in value) {
+      value = value[key];
+    } else {
+      const fallback = locales["en"];
+      let fb = fallback;
+      for (const k of keys) {
+        if (fb && typeof fb === "object" && k in fb) {
+          fb = fb[k];
+        } else {
+          return keyPath;
+        }
+      }
+      value = fb;
+      break;
+    }
+  }
+
+  if (typeof value !== "string") return keyPath;
+
+  if (data && typeof data === "object") {
+    value = value.replace(/\${(\w+)}/g, (_, key) => {
+      return data[key] !== undefined ? data[key] : `\${${key}}`;
+    });
+  }
+
+  return value;
+}
+
+module.exports = { t };
