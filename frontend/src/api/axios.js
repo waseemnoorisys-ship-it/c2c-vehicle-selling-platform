68396a245375c18aaada385ec5799941a45e20ee
waseemnoorisys-ship-it <waseem.noorisys@gmail.com>
2026-08-12 18:09:11 +0530

    adding backend url in axios

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import useAuthStore from "../store/useAuthStore";
 
 const api = axios.create({
-  baseURL: "/api/v1",
+  baseURL: import.meta.env.VITE_API_URL || "/api/v1",
   headers: { "Content-Type": "application/json" },
 });
 
